50997273a59a1a837ab808d83ee7c2a5cd03e689
lucascaravia <88347319+lucascaravia@users.noreply.github.com>
2023-11-21 10:17:06 -0300

    actualizacion de proyecto
    se toco el index , el sw y mas archivo para solucionar errores

---
+++ 2doParcialPWA/js/app.js
@@ -0,0 +1,236 @@
+const API = 'https://rickandmortyapi.com/api';
+
+var favoritos = [];
+var cardsApi = [];
+var cardApi = {};
+
+const getCards = async () => {
+    try {
+        const response = await fetch(`${API}/character`);
+        const data = await response.json();
+        return data.results;
+    } catch (error) {
+        console.log(error);
+
+    }
+};
+
+function chooseRandomElements(array, qty) {
+    const chooseElements = [];
+    console.log(array)
+
+    // Asegurarse de que la cantidad solicitada sea menor o igual al tamaño del array
+    if (qty > array.length) {
+        console.error("La cantidad solicitada es mayor que el tamaño del array.");
+        return [];
+    }
+
+    while (chooseElements.length < qty) {
+        // Generar un índice aleatorio
+        const randomIndex = Math.floor(Math.random() * array.length);
+
+        // Verificar si este índice ya ha sido elegido previamente
+        if (chooseElements.indexOf(array[randomIndex]) === -1) {
+            chooseElements.push(array[randomIndex]);
+        }
+    }
+
+    return chooseElements;
+}
+
+const searchCards = async (name) => {
+    try {
+        const response = await fetch(`${API}/character?name=${name}`);
+        const data = await response.json();
+        return data.results;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const cardDetail = async (id) => {
+    try {
+        const response = await fetch(`${API}/character/${id}`);
+        const data = await response.json();
+        return data
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+
+        function obtainDetail(id) {
+            let loader = document.querySelector('.loader')
+            loader.classList.remove('d-none');
+            cardApi = cardDetail(id)
+                .then(Object => {
+                    cardApi = Object;
+                    document.querySelector('#errorApi').classList.add('d-none');
+
+                    const cardElement = document.querySelector('#detailModal');
+                  
+                    cardElement.innerHTML = `
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h1 class="modal-title fs-5 text-dark" id="detailModalLabel">${Object.name}</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body py-2">
+                                <ul class="list-group mb-2">
+                                    <li class="list-group-item"><span class="fw-bold">Genero :</span> ${Object.gender}</li>
+                                    <li class="list-group-item"><span class="fw-bold">Id:</span> ${Object.id}</li>
+                                    <li class="list-group-item"><span class="fw-bold">Especie:</span> ${Object.species}</li>
+                                </ul>
+                                <div class="mb-2 d-flex justify-content-center">
+                                    <img class="card-img-top w-50" src="${Object.image}" alt="${Object.name}">
+                                </div>
+                                <p>${Object.status}</p>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="button" onclick="addFavorito()" class="btn btn-primary" id="#favotitoButton">Agregar a Favoritos</button>
+                            </div>
+                        </div>
+                    </div>`;
+
+                    loader.classList.add('d-none');
+
+                    const myModal = new bootstrap.Modal('#detailModal', {});
+                    myModal.show();
+                })
+                .catch(error => {
+                    console.log(error);
+                    loader.classList.add('d-none');
+                    document.querySelector('#errorApi').classList.remove('d-none');
+                });
+
+        }
+
+function search() {
+    console.log(document.querySelector('#search').value)
+    cardsApi = searchCards(document.querySelector('#search').value)
+        .then(cards => {
+            if (cards.length === 0) {
+                document.querySelector('#nothingFound').classList.remove('d-none');
+                return;
+            }else{
+                document.querySelector('#nothingFound').classList.add('d-none');
+                document.querySelector('.cards').innerHTML = "";
+                cards.forEach(Object => {
+                    if (!(Object.id === undefined)) {
+                        const cardElement = document.createElement('div');
+                        cardElement.className = 'card col-2 p-2';
+                        cardElement.innerHTML = `
+                            <img class="card-img-top" src="${Object.image}" alt="${Object.name}">
+                            <h5 class="card-title text-dark">${Object.name}</h5>
+                            <p class="card-text text-dark">${Object.type}</p>
+                            <button  onclick="obtainDetail(${Object.id})" class="btn btn-primary">Card Detail</button>
+                        `;
+                        document.querySelector('.cards').appendChild(cardElement);
+                    }
+                });
+            }
+        })
+        .catch(error => console.log(error))
+        .finally(() => console.log('Cards searched loaded'));
+}
+
+function addFavorito() {
+    favoritos = JSON.parse(localStorage.getItem('favoritos')) || [];
+    console.log(favoritos);
+    if (favoritos.length === 0) {
+        favoritos.push(cardApi);
+        localStorage.setItem('favoritos', JSON.stringify(favoritos));
+        console.log(JSON.parse(localStorage.getItem('favoritos')));
+        return;
+    }
+    favoritos.forEach(favotito => {
+        if(favotito.id === cardApi.id){
+            alert('Esta carta ya se encuentra en favoritos');
+            return;
+        }else{
+            favoritos.push(cardApi);
+            localStorage.setItem('favoritos', JSON.stringify(favoritos));
+            console.log(JSON.parse(localStorage.getItem('favoritos')));
+        }
+    });
+}
+
+function callAllCards(){
+    cardsApi = getCards()
+        .then(cards => {
+            document.querySelector('#errorApi').classList.add('d-none');
+            console.log(cards)
+            let chooseCards = chooseRandomElements(cards, 10);
+            chooseCards.forEach(card => {
+                if (!(card.id === undefined)) {
+                    const cardElement = document.createElement('div');
+                    cardElement.className = 'card col-2 p-2';
+                    cardElement.innerHTML = `
+                        <img class="card-img-top" src="${card.image}" alt="${card.name}">
+                        <h4 class="card-title text-dark">${card.name}</h4>
+                        <p class="card-text text-dark">${card.type}</p>
+                        <button  onclick="obtainDetail(${card.id})" class="btn btn-primary">Card Detail</button>
+                    `;
+                    document.querySelector('.cards').appendChild(cardElement);
+                }
+            });
+        })
+        .catch(error => {
+            document.querySelector('#errorApi').classList.remove('d-none');
+            console.log(error)
+        })
+        .finally(() => console.log('Cards loaded'));
+}
+
+function deleteFavorito(id) {
+    favoritos = JSON.parse(localStorage.getItem('favoritos')) || [];
+    favoritos = favoritos.filter(function(card){
+        return  card.id != id;
+    });
+    console.log(favoritos)
+    localStorage.setItem('favoritos', JSON.stringify(favoritos));
+    showFavoritos();
+};
+
+function deleteAllfavoritos() {
+    localStorage.setItem('favoritos', JSON.stringify([]));
+    showFavoritos();
+}
+
+function showFavoritos() {
+    favoritos = JSON.parse(localStorage.getItem('favoritos')) || [];
+    if (favoritos.length === 0) {
+        document.querySelector('#nothingFound').classList.remove('d-none');
+        document.querySelector('.cards').innerHTML = "";
+        return;
+    }else{
+        document.querySelector('#nothingFound').classList.add('d-none');
+        document.querySelector('.cards').innerHTML = "";
+        favoritos.forEach(card => {
+            const cardElement = document.createElement('div');
+                    cardElement.className = 'card col-2 p-2';
+                    cardElement.innerHTML = `
+                        <img class="card-img-top" src="${card.image}" alt="${card.name}">
+                        <h5 class="card-title text-dark>${card.name}</h3>
+                        <p class="card-text text-dark">${card.type}</p>
+                        <button  onclick="deletefavotito(${card.id})" class="btn btn-danger">Delete</button>
+                    `;
+                    document.querySelector('.cards').appendChild(cardElement);
+        });
+    }
+};
+
+
+window.addEventListener('load', () => {
+
+        const route = window.location.pathname;
+        if (route.includes('index.html')) {
+            callAllCards();
+        }else if (route.includes('favoritos.html')) {
+            showFavoritos();
+        }else{
+            console.log('no estaria funcionando');
+        }
+});
---
+++ 2doParcialPWA/js/script.js
@@ -0,0 +1,140 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function () {
+        navigator.serviceWorker.register('sw.js')
+            .then(function (registration) {
+                // Registro ok
+                console.log('El registro del ServiceWorker fue exitoso, tiene el siguiente alcance: ', registration.scope);
+                // console.log(registration);
+            })
+            .catch(function (err) {
+                // registro falló :(
+                console.log('El registro del ServiceWorker falló: ', err);
+            });
+
+        // Demorar el pop up del permiso para notificar
+        if (window.Notification && Notification.permission !== 'denied') {
+            setTimeout(function () {
+                Notification.requestPermission();
+            }, 10000);
+
+            var noti = new Notification("titulo", {
+                body: "Soy una notificacion",
+                icon: "android-icon-192x192.png",
+                image: "https://cdn.akamai.steamstatic.com/steam/apps/2141910/capsule_616x353.jpg?t=1697070931",
+                badge: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvywGPax9oaCFHKkTXC8rrR9MZMGcHzq3XuVseKT1jrxkoGaqK"
+            });
+        }
+
+        //detectar estado de conexion
+
+        (function () {
+            var header = document.querySelector(".encabezado"),
+                metaTema = document.querySelector("meta[name=theme-color]");
+
+
+            function estado(e) {
+                console.log(e.type) //type es una propiedad que devuelve el estado
+
+                if (navigator.onLine) {
+                    metaTema.setAttribute("content", "#00ff00")
+                    header.classList.remove("offline")
+
+                    alert("Hay red");
+
+                } else {
+                    metaTema.setAttribute("content", "##D90000")
+                    header.classList.add("offline")
+
+                    alert("Sin red");
+                }
+
+            }//fin estado
+
+            if (!navigator.onLine) {
+                estado(this)
+            }
+            window.addEventListener("online", estado);
+            window.addEventListener("offline", estado);
+
+
+        })();
+
+        //API Share
+        (function () {
+
+            document.querySelector('.share').addEventListener('click', function () {
+
+                if (navigator.share) {
+                    navigator.share({
+                        title: 'PWA APIS',
+                        text: 'Ejemplo de api share ',
+                        url: 'rickymortyapipractica.netlify.app',
+                    })
+                        .then(function () {
+                            console.log("Se compartió")
+
+                        })
+                        .catch(function (error) {
+                            console.log(error)
+
+
+                        })
+                }
+            });
+
+        })()
+
+    }); //load
+
+}
+
+//funcion anónima auto-ejecutable. Todo lo que hay dentro está aislado del resto del código de la página. Agregar a pantalla de inicio
+(function () {
+
+    var aviso;
+
+    window.addEventListener('beforeinstallprompt', function (e) {
+        e.preventDefault();
+        aviso = e;
+        console.log(aviso)
+
+        showAddToHomeScreen();
+
+    });
+
+    function showAddToHomeScreen() {
+        var showAlerts = document.querySelector(".agregar-alerta");
+        if (showAlerts != undefined) {
+            showAlerts.style.display = "flex";
+            showAlerts.addEventListener("click", addToHomeScreen);
+        }
+    }
+
+    function addToHomeScreen() {
+        var showAlerts = document.querySelector(".agregar-alerta");
+        showAlerts.style.display = 'none';
+
+        if (aviso) {
+            aviso.prompt();
+            aviso.userChoice
+                .then(function (choiceResult) {
+
+                    if (choiceResult.outcome === 'accepted') {
+                        console.log('El usuario acepto las notis');
+                    } else {
+                        console.log('El usuario rechazó las notis');
+                    }
+
+                    aviso = null;
+
+                });
+
+        }
+
+    }
+
+    showAddToHomeScreen();
+
+})(); //Ejecutamos la función
+
+
